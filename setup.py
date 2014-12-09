a74623008cbfa8d224ee187ff864e51dd1975b85
Brian Drawert <bdrawert@cs.ucsb.edu>
2014-12-09 14:46:01 -0800

    Install script magic

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
 from setuptools import setup
-import subprocess
+from setuptool.command.install import install
+import os
+
+class Install(install):
+    def do_egg_install(self):
+        cmd = "echo 'import os' > gillespy/__init__.py'"
+        if os.envion.get('STOCHSS_HOME') is not None:
+            cmd += "echo 'os.environ[\'PATH\'] += os.pathsep + {0}' > gillespy/__init__.py".format(os.environ['STOCHSS_HOME'])
+        if os.envion.get('STOCHKIT_HOME') is not None:
+            cmd += "echo 'os.environ[\'PATH\'] += os.pathsep + {0}' > gillespy/__init__.py".format(os.environ['STOCHKIT_HOME'])
+        if os.envion.get('STOCHKIT_ODE_HOME') is not None:
+            cmd += "echo 'os.environ[\'PATH\'] += os.pathsep + {0}' > gillespy/__init__.py".format(os.environ['STOCHKIT_ODE_HOME'])
+        print cmd
+        self.run_command(cmd)
+        install.do_egg_install(self)
 
 
-subprocess.call("./install-stochkit.sh")
 
 setup(name = "gillespy",
-      version = "0.0.1",
+      version = "0.1",
       packages = ['gillespy'],
       description = 'Python interface to the Gillespie StochKit2 solvers',
       
