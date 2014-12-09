5f4ab0a6b9d862caeca32d32263bc8ecd2c78faf
Brian Drawert <bdrawert@cs.ucsb.edu>
2014-12-09 14:53:27 -0800

    setup raises exception if StochKit is not found

--- setup.py
+++ setup.py
@@ -4,14 +4,20 @@ import os
 
 class Install(install):
     def do_egg_install(self):
-        cmd = "echo 'import os' > gillespy/__init__.py'"
+        cmd = "echo 'import os' > gillespy/__init__.py'\n"
+        success = False
         if os.envion.get('STOCHSS_HOME') is not None:
-            cmd += "echo 'os.environ[\'PATH\'] += os.pathsep + {0}' > gillespy/__init__.py".format(os.environ['STOCHSS_HOME'])
+            success = True
+            cmd += "echo 'os.environ[\'PATH\'] += os.pathsep + {0}' > gillespy/__init__.py\n".format(os.environ['STOCHSS_HOME'])
         if os.envion.get('STOCHKIT_HOME') is not None:
-            cmd += "echo 'os.environ[\'PATH\'] += os.pathsep + {0}' > gillespy/__init__.py".format(os.environ['STOCHKIT_HOME'])
+            success = True
+            cmd += "echo 'os.environ[\'PATH\'] += os.pathsep + {0}' > gillespy/__init__.py\n".format(os.environ['STOCHKIT_HOME'])
         if os.envion.get('STOCHKIT_ODE_HOME') is not None:
-            cmd += "echo 'os.environ[\'PATH\'] += os.pathsep + {0}' > gillespy/__init__.py".format(os.environ['STOCHKIT_ODE_HOME'])
+            success = True
+            cmd += "echo 'os.environ[\'PATH\'] += os.pathsep + {0}' > gillespy/__init__.py\n".format(os.environ['STOCHKIT_ODE_HOME'])
         print cmd
+        if not success:
+            raise Exception("StochKit not found, to simulation GillesPy models either StochSS or StochKit needs to be installed")
         self.run_command(cmd)
         install.do_egg_install(self)
 
