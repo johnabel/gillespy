8b9862104252f34a82dd570cee9193d671ec7467
Brian Drawert <bdrawert@cs.ucsb.edu>
2014-12-11 14:53:05 -0800

    setup.py from johnbranch

--- setup.py
+++ setup.py
@@ -1,65 +1,69 @@
-import setuptools
 from setuptools import setup
+from setuptools.command.develop import develop
 from setuptools.command.install import install
-import setuptools.command.bdist_egg
-import os
-import sys
+from setuptools.command.bdist_egg import bdist_egg
+from setuptools.command.easy_install import easy_install
 import subprocess
+import os
 
 SETUP_DIR = os.path.dirname(os.path.abspath(__file__))
 
-
-#class Do_bdist_egg(setuptools.command.bdist_egg):
-#    def run(self):
-#        self.run_command('build_gillespy')
-#        setuptools.command.bdist_egg.run(self)
-
-class Install(install):
-    def do_egg_install(self):
-        print "Install.do_egg_install"
-        self.run_command('build_gillespy')
-        install.do_egg_install(self)
-
-    def run(self):
-        print "Install.run"
-        self.run_command('build_gillespy')
-        install.run(self)
-
-class GillesPyBuild(setuptools.Command):
-    description = 'Configure GillesPy to find solvers'
-
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        print "GillesPyBuild.run()"
+def stoch_path(command_subclass):
+    """
+    A decorator for classes subclassing one of the setuptools commands.
+    It modifies the run() method.
+    """
+    orig_run = command_subclass.run
+    
+    def modified_run(self):
+      
         success=False
-        cmd = "echo 'from .gillespy import *' > gillespy/__init__.py"
-        cmd += "\necho 'import os' >> gillespy/__init__.py"
+        cmd = "echo 'from .gillespy import *' > {0}/gillespy/__init__.py".format(SETUP_DIR)
+        cmd += "\necho 'import os' >> {0}/gillespy/__init__.py".format(SETUP_DIR)
         if os.environ.get('STOCHSS_HOME') is not None:
-            cmd += "\necho 'os.environ[\"PATH\"] += os.pathsep + \"{0}/StochKit/\"' >> gillespy/__init__.py".format(os.environ['STOCHSS_HOME'])
-            cmd += "\necho 'os.environ[\"PATH\"] += os.pathsep + \"{0}/ode/\"' >> gillespy/__init__.py".format(os.environ['STOCHSS_HOME'])
+            cmd += "\necho 'os.environ[\"PATH\"] += os.pathsep + \"{0}/StochKit/\"' >> {1}/gillespy/__init__.py".format(os.environ['STOCHSS_HOME'],SETUP_DIR)
+            cmd += "\necho 'os.environ[\"PATH\"] += os.pathsep + \"{0}/ode/\"' >> {1}/gillespy/__init__.py".format(os.environ['STOCHSS_HOME'],SETUP_DIR)            
             success=True
         if os.environ.get('STOCHKIT_HOME') is not None:
-            cmd += "\necho 'os.environ[\"PATH\"] += os.pathsep + \"{0}\"' >> gillespy/__init__.py".format(os.environ['STOCHKIT_HOME'])
+            cmd += "\necho 'os.environ[\"PATH\"] += os.pathsep + \"{0}\"' >> {1}/gillespy/__init__.py".format(os.environ['STOCHKIT_HOME'],SETUP_DIR)
             success=True
         if os.environ.get('STOCHKIT_ODE_HOME') is not None:
-            cmd += "\necho 'os.environ[\"PATH\"] += os.pathsep + \"{0}\"' >> gillespy/__init__.py".format(os.environ['STOCHKIT_ODE_HOME'])
+            cmd += "\necho 'os.environ[\"PATH\"] += os.pathsep + \"{0}\"' >> {1}/gillespy/__init__.py".format(os.environ['STOCHKIT_ODE_HOME'],SETUP_DIR)
             success=True
-        print cmd
-        try:
-            subprocess.check_call(cmd, shell=True)
-        except (subprocess.CalledProcessError, OSError) as e:
-            sys.stderr.write('Problems setting path to solvers in GillesPy: {0}'.format(e))
-            raise SystemExit
         if success is False:
            raise Exception("StochKit not found, to simulate GillesPy models either StochKit solvers or StochSS must to be installed")
         
+        try:
+            subprocess.check_call(cmd,shell=True)
+        except(subprocess.CalledProcessError,OSError) as e:
+            print "It didn't work {0}".format(e)
+            raise SystemExit
+            
+                
+        orig_run(self)
+        print str(orig_run)
+    command_subclass.run = modified_run
+    return command_subclass
+
+
+
+# update all install classes with our new class
+@stoch_path
+class develop_new(develop):
+    pass
+
+@stoch_path
+class install_new(install):
+    pass
+
+@stoch_path
+class bdist_egg_new(bdist_egg):
+    pass
+
+@stoch_path
+class easy_install_new(easy_install):
+    pass
+
 
 
 
@@ -81,11 +85,9 @@ setup(name = "gillespy",
 
       download_url = "https://github.com/JohnAbel/GillesPy/tarball/master/",
       
-      cmdclass = {
-         #'build' : Do_build,
-         #'bdist_egg' : Do_bdist_egg,
-         'build_gillespy': GillesPyBuild,
-         'install':Install
-      }
+      cmdclass = {'bdist_egg':bdist_egg_new,
+                  'install':install_new,
+                  'develop':develop_new,
+                  'easy_install':easy_install_new}
       
       )
