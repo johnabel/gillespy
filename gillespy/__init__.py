cad0244fc4397142b6f9a6f67c906e5e61662a01
Brian Drawert <bdrawert@cs.ucsb.edu>
2017-04-25 12:33:03 -0700

    removed paths

--- gillespy/__init__.py
+++ gillespy/__init__.py
@@ -1,5 +1,2 @@
 from .gillespy import *
 import os
-#os.environ["PATH"] += os.pathsep + "/Users/brian/Desktop/research/StochKit/"
-os.environ["PATH"] += os.pathsep + "/Users/brian/Desktop/research/GillesPy/StochKit/"
-os.environ["PATH"] += os.pathsep + "/Users/brian/Desktop/research/StochKit_ode/"
